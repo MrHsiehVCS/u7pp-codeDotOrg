30097ff0a73b07f86baa457c9a56e5c0e9ce63ff
Eric Hsieh <erich@villagechristian.org>
2023-03-06 02:02:35 -0800

    cleanup main

--- src/main/java/u7pp/Main.java
+++ src/main/java/u7pp/Main.java
@@ -4,9 +4,6 @@ package u7pp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to u7pp");
-
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(RecursiveFunctions.getBinary(16));
+        System.out.println("Welcome to u7pp :)\nUse this space to test your code.");
     }
 }
